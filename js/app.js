73c6f624dff537986795c3e13edd21b6bad5e626
wlefrancois <135740477+wlefrancois@users.noreply.github.com>
2026-03-05 06:40:32 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-// js/app.js
 import { createClient } from "/js/vendor/supabase.js";
 
 const SUPABASE_URL = "https://ridxjmwuepoaefidacda.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_OB9EHyTG5q86PNJpgXB89w_g_VzCV2Y";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
 
 export async function getUser() {
   const { data } = await supabase.auth.getUser();
@@ -20,10 +22,12 @@ export async function signUp(email, password) {
     }
   });
 }
-}
 
 export async function signIn(email, password) {
-  return supabase.auth.signInWithPassword({ email, password });
+  return supabase.auth.signInWithPassword({
+    email,
+    password
+  });
 }
 
 export async function signOut() {
