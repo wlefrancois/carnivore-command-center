bfdb74841b98f258d6f80874b10f655c0047aa09
wlefrancois <135740477+wlefrancois@users.noreply.github.com>
2026-03-06 10:22:44 -0600

    Update chart.js

--- js/vendor/chart.js
+++ js/vendor/chart.js
@@ -1,1 +1,5 @@
-export * from "https://cdn.jsdelivr.net/npm/chart.js@4.4.0/+esm";
+import { Chart, registerables } from "https://cdn.jsdelivr.net/npm/chart.js@4.4.0/+esm";
+
+Chart.register(...registerables);
+
+export { Chart };
