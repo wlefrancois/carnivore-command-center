e4a678fb710d4efdc43c9329644c1accd31c3663
wlefrancois <wayne@ciscomaster.com>
2026-05-17 08:25:20 -0500

    remove dist from repo

--- dist/js/supabase.js
+++
@@ -1,2 +0,0 @@
-// js/vendor/supabase.js
-export { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
--- dist/js/vendor/chart.js
+++
@@ -1,5 +0,0 @@
-import { Chart, registerables } from "https://cdn.jsdelivr.net/npm/chart.js@4.4.0/+esm";
-
-Chart.register(...registerables);
-
-export { Chart };
